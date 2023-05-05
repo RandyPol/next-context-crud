436a3a9abe5418e513af8e561cb036c6cd6ce34d
Ran Pol <rnd809@gmail.com>
2023-05-05 19:34:07 -0400

    Create the initial files for the task app.

---
+++ src/app/edit/[id]/page.js
@@ -0,0 +1,5 @@
+function Page({ params }) {
+  return <div>Edit Pag {params.id}</div>
+}
+
+export default Page
---
+++ src/app/new/page.js
@@ -0,0 +1,6 @@
+function Page() {
+    return <div>Task Form</div>
+  }
+  
+  export default Page
+  
