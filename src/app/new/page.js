b15375b76c946ab3dc464032dba4d9a62b502b04
Ran Pol <rnd809@gmail.com>
2023-05-05 22:44:07 -0400

    feat: Create task form page with input fields and submit button

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -1,6 +1,34 @@
+'use client'
+
+import { useState } from 'react'
+
 function Page() {
-    return <div>Task Form</div>
+  const [task, setTask] = useState()
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setTask((prev) => ({
+      ...prev,
+      [name]: value,
+    }))
   }
-  
-  export default Page
-  
+  console.log(JSON.stringify(task))
+  return (
+    <form>
+      <input
+        name="title"
+        type="text"
+        placeholder="Write a title"
+        onChange={handleChange}
+      />
+      <textarea
+        name="description"
+        placeholder="Write a description"
+        onChange={handleChange}
+      />
+      <button type="submit">Create</button>
+    </form>
+  )
+}
+
+export default Page
