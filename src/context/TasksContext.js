976af45c6a6ac1af18d199a1e8d4e6d5cf1ae0f8
Ran Pol <rnd809@gmail.com>
2023-05-05 21:30:35 -0400

    Add initial notes to the context.

--- src/context/TasksContext.js
+++ src/context/TasksContext.js
@@ -13,7 +13,23 @@ const useTasks = () => {
 }
 
 const TasksProvider = ({ children }) => {
-  const tasks = []
+  const tasks = [
+    {
+      id: 1,
+      title: 'Task 1',
+      description: 'Description 1',
+    },
+    {
+      id: 2,
+      title: 'Task 2',
+      description: 'Description 2',
+    },
+    {
+      id: 3,
+      title: 'Task 3',
+      description: 'Description 3',
+    },
+  ]
   return (
     <TasksContext.Provider
       value={{
