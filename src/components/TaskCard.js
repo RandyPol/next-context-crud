8f63638fa7b8ba5007e578bd29d7d6fce6e35aaf
Ran Pol <rnd809@gmail.com>
2023-05-06 21:44:51 -0400

    Refactor TaskCard component to improve styling and readability

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -6,11 +6,6 @@ function TaskCard({ task }) {
   const router = useRouter()
   const { deleteTask } = useTasks()
 
-  const styleObj = {
-    backgroundColor: '#202020',
-    color: 'white',
-  }
-
   const handleDelete = (event) => {
     event.stopPropagation()
     const confirm = window.confirm(
@@ -21,12 +16,22 @@ function TaskCard({ task }) {
   }
 
   return (
-    <div style={styleObj} onClick={() => router.push(`/edit/${task.id}`)}>
-      <h1>{`${task.title} ID: ${task.id}`}</h1>
-      <button type="button" onClick={handleDelete}>
-        Delete
-      </button>
-      <p>{task.description}</p>
+    <div
+      className="bg-gray-700 hover:bg-slate-600 cursor-pointer px-20 py-5 m-2"
+      onClick={() => router.push(`/edit/${task.id}`)}
+    >
+      <div className="flex justify-between">
+        <h1>{`${task.title}`}</h1>
+        <button
+          className="bg-red-700 hover:bg-red-600 px-3 py-1 inline-flex items-center"
+          type="button"
+          onClick={handleDelete}
+        >
+          Delete
+        </button>
+      </div>
+      <p className="text-gray-300">{task.description}</p>
+      <span className="text-gray-400 text-xs">{task.id}</span>
     </div>
   )
 }
