7234aa06ebf8c6b0586a96bca0ac266d6e2147bd
Ran Pol <rnd809@gmail.com>
2023-05-06 22:13:16 -0400

    Refactor new task page form UI

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -47,19 +47,28 @@ function Page({ params }) {
   }, [params.id, tasks, setValue])
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        placeholder="Write a title"
-        {...register('title', { required: true })}
-      />
-      {errors.title && <span>This field is required</span>}
-      <textarea
-        placeholder="Write a description"
-        {...register('description', { required: true })}
-      />
-      {errors.description && <span>This field is required</span>}
-      <button type="submit">Create</button>
-    </form>
+    <div className="flex justify-center items-center h-full">
+      <form className="bg-gray-700 p-10" onSubmit={onSubmit}>
+        <input
+          className="bg-gray-800 py-3 px-4 mb-2 block focus:outline-none w-full  "
+          placeholder="Write a title"
+          {...register('title', { required: true })}
+        />
+        {errors.title && <span>This field is required</span>}
+        <textarea
+          className="bg-gray-800 py-3 px-4 mb-2 block focus:outline-none w-full  "
+          placeholder="Write a description"
+          {...register('description', { required: true })}
+        />
+        {errors.description && <span>This field is required</span>}
+        <button
+          className="bg-green-500 hover:bg-green-400 px-2 py-2 rounded-sm disabled:opacity-30"
+          type="submit"
+        >
+          Create
+        </button>
+      </form>
+    </div>
   )
 }
 
